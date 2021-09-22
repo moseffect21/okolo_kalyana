e3dfb9ff7d4a60a511570eb59dd59856efbc6076
moseffect21 <moseffect21@gmail.com>
2021-09-22 21:10:28 +0300

    add video & fix mobile about

--- resources/js/components/pages/MainPage/components/VideoSwiper.tsx
+++ resources/js/components/pages/MainPage/components/VideoSwiper.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/media-has-caption */
 import React from 'react'
 import SwiperCore, { Navigation } from 'swiper'
 import { Swiper, SwiperSlide } from 'swiper/react'
@@ -19,12 +20,25 @@ const VideoSwiper = () => {
       >
         <SwiperSlide>
           <div className={s.video_item}>
+            <video
+              className={s.img}
+              preload="auto"
+              autoPlay
+              loop
+              muted
+              poster="/videos/promo_main_poster.jpg"
+            >
+              <source
+                src="/videos/promo_main.mp4"
+                type='video/mp4; codecs="avc1.42E01E, mp4a.40.2"'
+              />
+            </video>
             <img src="/images/video1.png" alt="" className={s.img} />
-            <img src="/images/icons/play_icon.svg" className={s.play_icon} alt="" />
+            {/* <img src="/images/icons/play_icon.svg" className={s.play_icon} alt="" /> */}
             <div className={s.shadow} />
           </div>
         </SwiperSlide>
-        <SwiperSlide>
+        {/* <SwiperSlide>
           <div className={s.video_item}>
             <img src="/images/video2.png" alt="" className={s.img} />
             <img src="/images/icons/play_icon.svg" className={s.play_icon} alt="" />
@@ -43,7 +57,7 @@ const VideoSwiper = () => {
         </div>
         <div className={`button-prev ${s.button_prev}`} slot="container-end">
           <img src="/images/icons/next_white_arrow.svg" alt="prev" />
-        </div>
+        </div> */}
       </Swiper>
     </div>
   )
