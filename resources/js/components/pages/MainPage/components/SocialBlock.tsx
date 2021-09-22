ed1205d464e516872cdc03784f254e636e50f4de
moseffect21 <moseffect21@gmail.com>
2021-09-22 21:36:38 +0300

    target blank to soc links

--- resources/js/components/pages/MainPage/components/SocialBlock.tsx
+++ resources/js/components/pages/MainPage/components/SocialBlock.tsx
@@ -7,18 +7,35 @@ const SocialBlock = () => {
   return (
     <div className={s.container}>
       <div className={s.social_icons}>
-        <a href="https://www.instagram.com/okolo_kalyana/" className={`${s.social_item} ${s.inst}`}>
+        <a
+          href="https://www.instagram.com/okolo_kalyana/"
+          className={`${s.social_item} ${s.inst}`}
+          target="_blank"
+          rel="noreferrer"
+        >
           <img src="/images/icons/inst.svg" alt="" />
         </a>
-        <a href="https://vk.com/blog_hookah" className={`${s.social_item} ${s.vk}`}>
+        <a
+          href="https://vk.com/blog_hookah"
+          className={`${s.social_item} ${s.vk}`}
+          target="_blank"
+          rel="noreferrer"
+        >
           <img src="/images/icons/vk.svg" alt="" />
         </a>
-        <a href="https://t.me/okolokalyana" className={`${s.social_item} ${s.tg}`}>
+        <a
+          href="https://t.me/okolokalyana"
+          className={`${s.social_item} ${s.tg}`}
+          target="_blank"
+          rel="noreferrer"
+        >
           <img src="/images/icons/telegram.svg" alt="" />
         </a>
         <a
           href="https://www.youtube.com/c/%D0%92%D0%BB%D0%B0%D0%B4%D0%B8%D1%81%D0%BB%D0%B0%D0%B2%D0%9D%D0%BE%D1%81%D0%B0%D1%87%D0%B5%D0%B2"
           className={`${s.social_item} ${s.youtube}`}
+          target="_blank"
+          rel="noreferrer"
         >
           <img src="/images/icons/youtube2.svg" alt="" />
         </a>
