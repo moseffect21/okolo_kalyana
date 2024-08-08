9b2724b4a897bfd003390394bdf4cbad12f8ae51
moseffect21 <moseffect21@gmail.com>
2024-08-08 23:03:47 +0300

    upgrade laravel to 10.x

--- app/Http/Controllers/ReactController.php
+++ app/Http/Controllers/ReactController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use Nesk\Puphpeteer\Puppeteer;
 use Nesk\Rialto\Data\JsFunction;
 
 class ReactController extends Controller
@@ -20,36 +19,6 @@ class ReactController extends Controller
                 && preg_match('/bot|crawl|slurp|spider|mediapartners/i', $_SERVER['HTTP_USER_AGENT']));
         }
 
-        if (_bot_detected()) {
-
-            $puppeteer = new Puppeteer;
-            $browser = $puppeteer->launch(['args' => [
-                '--no-sandbox',
-                '--disable-setuid-sandbox',
-                '--disable-dev-shm-usage',
-                '--disable-accelerated-2d-canvas',
-                '--no-first-run',
-                '--no-zygote',
-                '--single-process', // <- this one doesn't works in Windows
-                '--disable-gpu',
-            ], 'headless' => true, 'ignoreHTTPSErrors' => true]);
-
-            $page = $browser->newPage();
-            // $page->setDefaultNavigationTimeout(0);
-            $page->goto(strval($request->url()));
-            // $page->waitForTimeout(10000);
-
-
-            $data = $page->evaluate(JsFunction::createWithBody('return document.documentElement.outerHTML'));
-            $data = str_replace(mix('js/app.js'), '', $data);
-            $yandex = mb_strcut($data, stripos($data, '<!-- Yandex.Metrika counter -->'), stripos($data, '<!-- /Yandex.Metrika counter -->') - stripos($data, '<!-- Yandex.Metrika counter -->'));
-            $data = str_replace($yandex, '', $data);
-            $browser->close();
-
-            return response($data, 200);
-        } else {
-
-            return view('master');
-        }
+        return view('master');
     }
 }
